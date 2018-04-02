5b834f3cc908cd732e17f8878150f81d8bf5245b
iwan <iwan.kimpe+git@gmail.com>
2018-04-02 11:04:55 +0200

    mobile layout

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+    $('#menu-icon').on('click', function(){
+        $(this).toggleClass('close');
+        $(".header").toggleClass('mobile-header-expanded');
+    });
     $('#searchToggle').on('click', function(){
         $('#floating-search-wrapper').toggleClass('hidden');
     });
